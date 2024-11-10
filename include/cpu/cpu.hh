8c67432c54d8a968054e5edb2a914c8639ee03c7
Vorontsov Amadey <89128818+vorontsov-amd@users.noreply.github.com>
2024-11-10 21:08:46 +0300

    Added loading the executable file into RAM (#2)
    Added loading the executable file into RAM. Create RAM, CPU classes.

---
+++ include/cpu/cpu.hh
@@ -0,0 +1,18 @@
+#pragma once
+#include "cpu/core.hpp"
+#include "ram/ram.hpp"
+#include "utils/warning.h"
+
+namespace simlinx {
+  class CPU final {
+    Core m_core;
+
+  public:
+    RAM m_ram;
+
+  public:
+    CPU(RAM::size_type ram_size) : m_ram(ram_size) {}
+
+    void run(Core::reg_t pc) { NOT_IMPLEMENTED(); };
+  };
+} // namespace simlinx
