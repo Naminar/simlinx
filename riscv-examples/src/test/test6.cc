856e2e8aa2c608d0dafe3fb40624073020c41192
Vorontsov Amadey <89128818+vorontsov-amd@users.noreply.github.com>
2024-11-20 23:22:36 +0300

    Support for other sections (#19)
    * Update sections
    * Applied git-clang-format

--- riscv-examples/src/test/test6.cc
+++ riscv-examples/src/test/test6.cc
@@ -1,8 +1,7 @@
 int fibonacci_recursive(int n) {
-    if (n <= 1) return 1;
-    return fibonacci_recursive(n-1) + fibonacci_recursive(n-1);
+  if (n <= 1)
+    return 1;
+  return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 1);
 }
 
-int main() {
-    return fibonacci_recursive(3);
-}
+int main() { return fibonacci_recursive(3); }
