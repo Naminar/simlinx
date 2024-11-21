47841d56617e83e2a22c32c78c63577916c209bc
Naminar <buravko.va@phystech.edu>
2024-11-21 14:32:21 +0300

    added possibility to access to the  regs via their names

--- generate/syscall/sysGen.py
+++ generate/syscall/sysGen.py
@@ -1,13 +1,17 @@
 from jinja2 import Template
 from sysParser import parse_linux_syscalls, parse_yaml
+from termcolor import colored
+import os
 
 template_cc = Template("""
+#include "syscall.gen.hh"
+
 {% for syscall in syscalls %}
 // {{ syscall.enum}}
 // args [{% for i in range(syscall.args|length) %} a{{ i }} {{ syscall.args[i] }}{{ ", " if not loop.last else "" }}{% endfor %}]
-void {{ syscall.name }}() {
+void {{ syscall.name }}(simlinx::Core& core) {
 {{ syscall.do | indent(4) }}
-    return void; 
+    return; 
 }
 {% endfor %}
 """)
@@ -15,12 +19,14 @@ void {{ syscall.name }}() {
 template_hh = Template("""
 #pragma once
 #include <functional>
+#include <stdint.h>
+#include <stdio.h>
 
 {% for syscall in syscalls %}
-void {{ syscall.name }}();
+void {{ syscall.name }}(simlinx::Core&);
 {% endfor %}
 
-using SyscallFunctionType = std::function<void()>;
+using SyscallFunctionType = std::function<void(simlinx::Core&)>;
 std::array<SyscallFunctionType, {{ syscalls|length }}> syscalls = {
     {% for syscall in syscalls %}
     {{ syscall.name }} {{ ", " if not loop.last else "" }}
@@ -40,8 +46,22 @@ for syscall in syscalls:
 output = template_cc.render(syscalls=syscalls)
 print(output)
 
-with open('syscall.gen.cc', 'w') as f:
+path = os.getcwd()
+if 'simlinx' in path:
+    path = path.split('simlinx')[0]
+    path += 'simlinx/'
+else:
+    print(colored('simlinx directory not found', 'red'))
+    exit()
+
+if not os.path.exists(path + 'src/syscall'):
+    print(colored('Director was not found, createing simlinx/src/syscall', 'yellow'))
+    os.makedirs(path + 'src/syscall')
+with open(path + 'src/syscall/syscall.gen.cc', 'w', encoding='utf-8') as f:
     f.write(output)
 
-with open('syscall.gen.hh', 'w') as f:
+if not os.path.exists(path + 'include/syscall'):
+    print(colored('Director was not found, createing simlinx/include/syscall', 'yellow'))
+    os.makedirs(path + 'include/syscall')
+with open(path + 'include/syscall/syscall.gen.hh', 'w', encoding='utf-8') as f:
     f.write(template_hh.render(syscalls=syscalls))
