9d93af487205b2ee7fc20eb1307a5861807867c1
Naminar <buravko.va@phystech.edu>
2024-11-21 13:07:45 +0300

    syscalls generation

--- generate/syscall/sysGen.py
+++ generate/syscall/sysGen.py
@@ -1,18 +1,47 @@
-import yaml
+from jinja2 import Template
+from sysParser import parse_linux_syscalls, parse_yaml
 
-def parse_yaml(file_path):
-    with open(file_path, 'r') as file:
-        try:
-            data = yaml.safe_load(file)
-            return data
-        except yaml.YAMLError as e:
-            print(f"Error parsing YAML file: {e}")
-            return None
+template_cc = Template("""
+{% for syscall in syscalls %}
+// {{ syscall.enum}}
+// args [{% for i in range(syscall.args|length) %} a{{ i }} {{ syscall.args[i] }}{{ ", " if not loop.last else "" }}{% endfor %}]
+void {{ syscall.name }}() {
+{{ syscall.do | indent(4) }}
+    return void; 
+}
+{% endfor %}
+""")
 
-if __name__ == "__main__":
-    yaml_file = 'syscalls.yaml'
-    parsed_data = parse_yaml(yaml_file)
+template_hh = Template("""
+#pragma once
+#include <functional>
 
-    if parsed_data:
-        print("Parsed YAML Data:")
-        print(parsed_data)
+{% for syscall in syscalls %}
+void {{ syscall.name }}();
+{% endfor %}
+
+using SyscallFunctionType = std::function<void()>;
+std::array<SyscallFunctionType, {{ syscalls|length }}> syscalls = {
+    {% for syscall in syscalls %}
+    {{ syscall.name }} {{ ", " if not loop.last else "" }}
+    {% endfor %}
+};
+""")
+
+
+syscalls=parse_yaml('syscalls.yaml')
+# print(syscalls)
+
+sys_enum = parse_linux_syscalls()
+
+for syscall in syscalls:
+    syscall['enum'] = sys_enum[syscall['name']]
+
+output = template_cc.render(syscalls=syscalls)
+print(output)
+
+with open('syscall.gen.cc', 'w') as f:
+    f.write(output)
+
+with open('syscall.gen.hh', 'w') as f:
+    f.write(template_hh.render(syscalls=syscalls))
--- generate/syscall/sysParser.py
+++ generate/syscall/sysParser.py
@@ -1,24 +1,45 @@
 import re
-with open('syscalls2.h', 'r') as f:
-    syscalls = f.readlines()
+import yaml
 
-syscall_start = None
-with open('unistd2.h', 'r') as f:
-    for line in f.readlines():
-        m = re.search('#define __.+ ([0-9]+)', line)
-        if m:
-            syscall_start = f'SYSCALL({m.group(1)},'
-            continue
+def parse_linux_syscalls():
+    with open('linux/syscalls.h', 'r') as f:
+        syscalls = f.readlines()
 
-        m = re.search('__S.+,(.+?)\)',line)
-        if m:
-            if not syscall_start:
+    syscall_start = None
+    syscalls_enum = dict()
+    with open('linux/unistd.h', 'r') as f:
+        for line in f.readlines():
+            m = re.search('#define __.+ ([0-9]+)', line)
+            if m:
+                syscall_start = f'SYSCALL({m.group(1)},'
                 continue
-            syscall = m.group(1).strip()
-            # a little fuction name preprocessing
-            syscall = re.sub('^compat_', '', syscall)
-            syscall = re.sub('^sys', '', syscall)
-            # find syscall declaration and print it out
-            definitions = [s for s in syscalls if f'{syscall}(' in s]
-            if len(definitions) == 0:
-                # syscall definition
+
+            m = re.search('__S.+,(.+?)\)',line)
+            if m:
+                if not syscall_start:
+                    continue
+                syscall = m.group(1).strip()
+                # a little fuction name preprocessing
+                syscall = re.sub('^compat_', '', syscall)
+                # syscall = re.sub('^sys_', '', syscall)
+                # find syscall declaration and print it out
+                definitions = [s for s in syscalls if f'{syscall}(' in s]
+                if len(definitions) == 0:
+                    # syscall definition not found!
+                    syscall_start = None
+                    continue
+                # print(f'{syscall_start}"{syscall}","{definitions[0].strip()}")')
+                syscalls_enum[syscall] = re.findall('[0-9]+',syscall_start)[-1]
+                syscall_start = None
+
+    # print(syscalls_enum)
+    return(syscalls_enum)
+
+def parse_yaml(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+            return None
