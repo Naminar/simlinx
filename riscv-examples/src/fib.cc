76e7fa4c25a6599e8204089ea31556701ed3398a
Amadey Vorontsov <vorontsov.amd@gmailcom>
2024-12-09 04:08:03 +0300

    Add Initialization of MMU

--- riscv-examples/src/fib.cc
+++ riscv-examples/src/fib.cc
@@ -1,7 +1,23 @@
+#include "../../page-table-api/sv39/sv39.hh"
+#include "../../page-table-api/type.hh"
+
+void InitMMU() {
+  Sv39PageTableBuilder builder;
+  builder.SetRootPPN(0x0);
+  builder.FirstLevelPtCnfg(0x10, 0x1, NextLevel);
+  builder.SecondLevelPtCnfg(0x10, 0x10, XO);
+  builder.SecondLevelPtCnfg(0x11, 0x11, RW);
+  builder.SecondLevelPtCnfg(0x4, 0x4, RW);
+  builder.EnableTranslation();
+}
+
 int fibonacci_recursive(int n) {
   if (n <= 1)
     return 1;
   return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
 
-int main() { return fibonacci_recursive(7); }
+int main() {
+  InitMMU();
+  return fibonacci_recursive(7); 
+}
